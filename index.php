ea7bacd589fb388094df2673b18cda9d7069afe9
stardustdestroy <jrfizan@gmail.com>
2017-07-05 21:32:26 -0500

    Bornita
    Primer commit

---
+++ index.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>Parallax Template - Materialize</title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="materialize/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="materialize/css/main.css" type="text/css" rel="stylesheet" />
+    <link rel="stylesheet" href="materialize/css/font-awesome-4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<!--          Menu         -->
+<nav class="nav-extended white top-menu" id="top-menu">
+    <div class="nav-wrapper " >
+      <a href="#" class="brand-logo">Logo</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li class="selected"><a class="" href="sass.html">Nosotros</a></li>
+        <li ><a class="" href="badges.html">Joyeria</a></li>
+        <li><a class="" href="collapsible.html">Hogar</a></li>
+        <li><a class="" href="collapsible.html">Tu diseño</a></li>
+        <li><a class="" href="collapsible.html">Unete</a></li>
+        <li><a class="" href="collapsible.html">Contáctanos</a></li>
+        <li><a href="mobile.html"><i class="material-icons">shopping_cart</i></a></li>
+        <li><a class="waves-effect waves-light btn">Login</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a class="" href="sass.html"><b>Nosotros</b></a></li>
+        <li><a class="" href="badges.html">Joyeria</a></li>
+        <li><a class="" href="collapsible.html">Hogar</a></li>
+        <li><a class="" href="collapsible.html">Tu diseño</a></li>
+        <li><a class="" href="collapsible.html">Unete</a></li>
+        <li><a class="" href="collapsible.html">Contáctanos</a></li>
+        <li><a class="" href="collapsible.html">Carrito</a></li>
+        <li><a class="" href="collapsible.html">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+
+<!---     Slider      -->
+<div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="http://lorempixel.com/1230/547/nature/6"> <!-- random image -->
+        <div class="caption left-align">
+          <h1>Joyeria <br>Artesanal</h1>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/1230/547/nature/7"> <!-- random image -->
+        <div class="caption left-align">
+           <h1>Joyeria <br>Artesanal</h1>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/1230/547/nature/10"> <!-- random image -->
+        <div class="caption left-align">
+           <h1>Joyeria <br>Artesanal</h1>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+<!---     Novedades      -->
+<div class="">
+    <div class="">
+    <div align="center">
+        <h5 class="selected"><a href="">NOVEDADES</a></h5>    
+    </div>
+
+    <!-- <div class="grid">
+          <div class="grid-sizer"></div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" />
+          </div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" />
+          </div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" />
+          </div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" />
+          </div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" />
+          </div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+          </div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/contrail.jpg" />
+          </div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/golden-hour.jpg" />
+          </div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
+          </div>
+          <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
+          </div>
+          <div class="grid-item grid-item--width2"></div>
+        </div> -->
+        <h1>Masonry - fitWidth</h1>
+
+<div class="grid">
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
+  </div>
+  <div class="grid-item grid-item--height2">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/golden-hour.jpg" />
+  </div>
+  <div class="grid-item grid-item--height3">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item grid-item--height2">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item grid-item--height2">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item grid-item--height3">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item grid-item--height2">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item grid-item--height2">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item grid-item--height2">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item grid-item--height3">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item grid-item--height2">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+  <div class="grid-item grid-item--height2">
+      <img class="responsive-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+  </div>
+</div>
+
+    </div>
+</div>
+<div class="">
+    <div class="section light-blue lighten-2">
+
+        <!--   Icon Section   -->
+        <div align="center">
+            <h5 class="selected-white"><a href="">VALORES</a></h5>  
+        </div><br>
+        <div class="row">
+            <div class="col s12 m6">             
+                <div class="row">
+                    <div class="col s6 right-align white-text">
+                       <i class="fa fa-check-square-o fa-5x" ></i>
+                   </div>
+                   <div class="left-align col s6">
+                       <br>
+                       <h5 class=" white-text">CALIDAD</h5>
+                   </div>
+               </div>
+            </div>
+
+            <div class="col s12 m6">             
+                <div class="row">
+                    <div class="col s6 right-align white-text">
+                        <i class="fa fa-heart-o  fa-5x" ></i>
+                   </div>
+                   <div class="left-align col s6">
+                       <br>
+                       <h5 class=" white-text">PASION</h5>
+                   </div>
+               </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col s12 m6">             
+                <div class="row">
+                    <div class="col s6 right-align white-text">
+                       <i class="fa fa-handshake-o fa-5x" ></i>
+                   </div>
+                   <div class="left-align col s6">
+                       <br>
+                       <h5 class=" white-text">HONRADEZ</h5>
+                   </div>
+               </div>
+            </div>
+
+            <div class="col s12 m6">             
+                <div class="row">
+                    <div class="col s6 right-align white-text">
+                      <i class="fa fa-lightbulb-o fa-5x" ></i>
+                   </div>
+                   <div class="left-align col s6">
+                       <br>
+                       <h5 class=" white-text">CREATIVIDAD</h5>
+                   </div>
+               </div>
+            </div>
+        </div>
+        <br>
+    </div>
+</div>
+<br>
+
+
+<div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+        <div class="container">
+            <div class="row center">
+                <h2 class="header col s12 white-text">Crea tu propio diseño</h2>
+            </div>
+            <div class="row center">
+                <a class="btn-start">PERSONALIZA</a>
+            </div>
+        </div>
+    </div>
+    <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 2"></div>
+</div>
+
+<div class="container">
+    <div class="section">
+
+        <div class="row">
+            <div class="col s12 center">
+                <h3><i class="mdi-content-send brown-text"></i></h3>
+                <h4>Contact Us</h4>
+                <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+        <div class="container">
+            <div class="row center">
+                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+            </div>
+        </div>
+    </div>
+    <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
+</div>
+
+<footer class="page-footer teal">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Company Bio</h5>
+                <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Settings</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Connect</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+        </div>
+    </div>
+</footer>
+
+
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="materialize/js/materialize.js"></script>
+<script src="materialize/js/init.js"></script>
+<script src="materialize/js/main.js"></script>
+<script src="materialize/js/masonry.pkgd.js"></script>
+
+</body>
+</html>
---
+++ materialize/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('.slider').slider();
+
+/*	var $grid = $('.grid').masonry({
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		//gutter:5,
+		columnWidth: '.grid-sizer'
+	});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+	$grid.masonry();
+});  
+*/
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 180,
+  isFitWidth: true
+});
+
+});
